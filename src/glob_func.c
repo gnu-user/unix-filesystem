dda4130afbace6c9c1661a224e6983064eca05f0
gnu_user <gsoc.student@gmail.com>
2012-11-18 17:15:37 -0500

    Added the complete concat function, needs testing
    Added the concat function, which is used to concatenate two arrays and return
    a destination pointer containing the results of the two. It will automatically
    realloc the appropriately sized array to store the results in.
    
    Note, in order to make it a generic for more than just one type it takes void pointers, it's a prime example of when static type systems break down in generic
    programming

--- src/glob_func.c
+++ src/glob_func.c
@@ -31,3 +31,50 @@ byte* copy_to_buf(byte* buf1, byte* buf2, uint32_t size1, uint32_t size2)
 	return buf2;
 }
 
+// NOTE a prime example of when static type systems break down in generic programming
+void* concat(void* dest, void* src, uint32_t size)
+{
+	/* UNDEFINED if dest OR src NULL */
+	if (dest == NULL || src == NULL)
+	{
+		return NULL;
+	}
+
+	/* Cast each pointer to a byte* for byte-by-byte concatenation
+	 * using pointer arithmetic
+	 */
+	uint32_t i = 0, j = 0;
+	byte* _dest = (byte*) dest;
+	byte* _src = (byte*) src;
+
+	/* Get the number of items in each array using the size as an index multiplier */
+	while (_dest[i * size] != NULL)
+	{
+		++i;
+	}
+	while (_src[j * size] != NULL)
+	{
+		++j;
+	}
+
+	/* Reallocate dest to contain the results using size as a multiplier */
+	dest = realloc(dest, ((i+j+1) * size));
+	_dest = (byte *) dest;
+
+	if (_dest != NULL)
+	{
+		/* Concatenate the two arrays using byte-by-byte concatenation */
+		for (uint32_t k = i * size; k < (i+j+1) * size; ++k)
+		{
+			_dest[k] = _src[k - (i * size)];
+		}
+	}
+	else
+	{
+		return NULL;
+	}
+
+	/* Free the memory used by src and return dest pointer */
+	free(src);
+	return dest;
+}
