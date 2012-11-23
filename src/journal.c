67466909185abba4aa7c8e56c2b931767a281dd2
Joseph <heron.joseph@gmail.com>
2012-11-23 14:56:15 -0500

    MODIFIED: journal to have empty functions
    Copied all of the functions declared in the journal header file into the c file and defaulted return to NULL, 0 or false.

--- src/journal.c
+++ src/journal.c
@@ -1,3 +1,39 @@
 /**
  *
  */
+
+#include "journal.h"
+
+journal* add_entry(journal_entry* entry, journal* buf) {
+
+	return NULL ;
+}
+
+static int sync_journal(journal *buf) {
+	return 0;
+}
+
+int flush_journal(journal *buf) {
+	return 0;
+}
+
+journal* read_journal(journal *buf) {
+	return NULL ;
+}
+
+static int link_fbl(uint32_t index) {
+	return 0;
+}
+
+static int link_inode(uint32_t dest_index, uint32_t inode_location) {
+	return 0;
+}
+
+static int remove_inode(uint32_t dest_index, uint32_t inode_location) {
+	return 0;
+}
+
+static bool is_journal_full(void)
+{
+	return false;
+}
