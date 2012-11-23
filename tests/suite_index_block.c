8531424b0176f87c303c9016a8f27d88d563de0b
drspangle <dsmullen@outbreaklabs.com>
2012-11-23 16:15:10 -0500

    added unit tests for index block functions

---
+++ tests/suite_index_block.c
@@ -0,0 +1,72 @@
+/*
+ * suite_index_block.c
+ *
+ *  Created on: Nov 23, 2012
+ *      Author: ubuntu
+ */
+
+#include "suite_index_block.h"
+#include "unit_tests.h"
+#include "../src/glob_data.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * generate_index test case
+ */
+int test_generate_index(void)
+{
+	/* Test 1 -- */
+
+	if (0)
+	{
+		test_fail("Unit Test Part 1");
+		return EXIT_FAILURE;
+	}
+	else
+	{
+		test_pass("Unit Test Part 1");
+	}
+
+	return EXIT_SUCCESS;
+}
+
+/**
+ * iterate_index test case
+ */
+int test_iterate_index(void)
+{
+	/* Test 1 -- */
+	if (0)
+	{
+		test_fail("Unit Test Part 1");
+		return EXIT_FAILURE;
+	}
+	else
+	{
+		test_pass("Unit Test Part 1");
+	}
+
+	return EXIT_SUCCESS;
+}
+
+/**
+ * calc_index_blocks test case
+ */
+int test_calc_index_blocks(void)
+{
+	/* Test 1 -- */
+	if (0)
+	{
+		test_fail("Unit Test Part 1");
+		return EXIT_FAILURE;
+	}
+	else
+	{
+		test_pass("Unit Test Part 1");
+	}
+
+	return EXIT_SUCCESS;
+}
+
