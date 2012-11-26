3af29c279764d462b5b103bee845bc2cc16db8bd
drspangle <dsmullen@outbreaklabs.com>
2012-11-26 14:08:41 -0500

    added and modified doxygen comments chopastickchopastickbootastrap

--- src/system_open_file_table.h
+++ src/system_open_file_table.h
@@ -6,11 +6,25 @@
 #ifndef SYSTEM_OPEN_FILE_TABLE_H_
 #define SYSTEM_OPEN_FILE_TABLE_H_
 
+
 /**
- * System wide open file table contains an array of file descriptors that will
- * be open.
+ * @struct swoft
  *
- * Needs to have at least 5 instances (to have 5 files open at once)
+ * @brief Pseudo-object containing the array of open file descriptors.
+ *
+ * @details The system wide open file table contains an array of
+ * file descriptors, which are locations of inodes that have been
+ * read from the disk into memory. These are used as handles to
+ * the files which exist on disk, and are synchronized with the
+ * data on disk upon modification. One instance needs to exist per file.
+ *
+ * @author Daniel Smullen
+ *
+ * @author Jon Gillett
+ *
+ * @author Joseph Heron
+ *
+ * @copyright GNU General Public License V3
  */
 typedef struct{
 	uint32_t fd[NUMOFL];
@@ -19,63 +33,137 @@ typedef struct{
 
 extern swoft system_open_tb;
 
+
 /**
- * Add the inode to the swoft
+ * @brief Add the inode to the system wide open file table.
  *
- * @param block_num location, the Inode's location on disk
+ * @details This function takes an inode's block location
+ * on disk, reads that block, and assigns the returned inode
+ * location on disk into memory to a file descriptor - this is
+ * an index on the system wide open file table.
  *
- * @param an integer value,
- * if the value >= 0 then the inode was found and added to the swoft
- * if the value == -1 then the swoft was full
- * if the value == -2 then the inode was not found
- * otherwise the function was unsuccessful
+ * @param block_num The inode's location on disk.
+ *
+ * @return Returns an integer value, if the value >= 0 then
+ * the inode was found and added to the system wide open
+ * file table. If the value = -1 then the table was full.
+ * If the value = -2 then the inode was not found.
+ * By default, any other return value signals that the
+ * function was unsuccessful.
+ *
+ * @author Daniel Smullen
+ *
+ * @author Jon Gillett
+ *
+ * @author Joseph Heron
+ *
+ * @copyright GNU General Public License V3
  */
 extern int add_to_swoft(uint32_t block_num);
 
+
 /**
- * validate whether the fd is a valid fd in the swoft
+ * @brief Validate whether the given file descriptor is
+ * a valid file descriptor in the system wide open file
+ * table.
  *
- * @param the file descriptor
+ * @param fd The file descriptor.
  *
- * @return an integer value,
- * if value >= 0 success
- * otherwise failure
+ * @return Returns an integer value, if the value >= 0
+ * the file descriptor was valid, otherwise the file
+ * descriptor was invalid, or the function was
+ * unsuccessful.
+ *
+ * @author Daniel Smullen
+ *
+ * @author Jon Gillett
+ *
+ * @author Joseph Heron
+ *
+ * @copyright GNU General Public License V3
  */
 extern int validate_fd(int fd);
 
+
 /**
- * Get the inode from the swoft
+ * @brief Get an inode instance from the system wide open
+ * file table.
  *
- * @param the file descriptor
+ * @details This function takes a file descriptor, which is
+ * an index on the system wide open file table. If there is
+ * an inode pseudo-object at the index specified, it is
+ * returned.
  *
- * @return the inode from the swoft given the file descriptor
+ * @param fd The file descriptor.
+ *
+ * @return Returns the inode instance from the system
+ * wide open file table given the file descriptor.
+ *
+ * @author Daniel Smullen
+ *
+ * @author Jon Gillett
+ *
+ * @author Joseph Heron
+ *
+ * @copyright GNU General Public License V3
  */
 extern inode get_swoft_inode(int fd);
 
+
 /**
- * Get the location of the inode on disk
+ * @brief Get the location of the inode on
+ * disk which corresponds to a file descriptor.
  *
- * @parem the file descriptor
+ * @details This function takes a file descriptor, and
+ * if there is a valid inode pseudo-object at that index
+ * on the system wide open file table, the location of
+ * that object on disk is returned from accessing
+ * it within the pseudo-object.
  *
- * @return the location of the inode on disk
+ * @param fd The file descriptor.
+ *
+ * @return Returns the location of the inode on disk.
+ *
+ * @author Daniel Smullen
+ *
+ * @author Jon Gillett
+ *
+ * @author Joseph Heron
+ *
+ * @copyright GNU General Public License V3
  */
 extern uint32_t get_inode_loc(int fd);
 
+
 /**
- * remove the fd from the swoft
+ * @brief Remove a file descriptor from the system wide open
+ * file table.
  *
- * @param the file descriptor to remove
+ * @details This function accesses an index on the system
+ * wide open file table, and frees whatever data is in
+ * memory corresponding to that index.
+ *
+ * @param fd The file descriptor to remove.
  */
 extern void remove_fd(int fd);
 
+
 /**
- * Find all the swoft entries that have the same entry as the given location and
- * remove it from the swoft
- * @param inode_location the location of the inode
+ * @brief Find all the system wide open file table indices that have
+ * the same data as the given location on disk, and remove them from
+ * the system wide open file table.
  *
- * @return an integer value indicating the number of entries deleted from the
- * swoft
+ * @details This function is used when deleting files. In order to
+ * remove all open handles to a file when it is removed from disk,
+ * the system wide open file table must have all corresponding
+ * handles removed.
+ *
+ * @param inode_location The location of the inode on disk.
+ *
+ * @return Returns an integer value indicating the number of
+ * entries deleted from the system wide open file table.
  */
 extern uint32_t find_and_remove(uint32_t inode_location);
+
 
 #endif /* SYSTEM_OPEN_FILE_TABLE_H_ */
