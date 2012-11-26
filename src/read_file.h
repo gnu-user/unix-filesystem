9431c62d8ccc85679269b7a34129297a8dc0bf42
drspangle <dsmullen@outbreaklabs.com>
2012-11-26 04:58:45 -0500

    updated the doxygen comments

--- src/read_file.h
+++ src/read_file.h
@@ -10,27 +10,41 @@
 #ifndef READ_FILE_H_
 #define READ_FILE_H_
 
-/** sfs_read
- * Copy the length of bytes of data specified from a regular file to memory
+
+/**
+ * @brief Read the contents of a file.
+ *
+ * @details Copy the length of bytes of data specified from a regular file to memory
  * location specified by mem_pointer. The parameter start gives the offset
  * of the first byte in the file that should be copied.
  *
- * @param fd A file descriptor for the file to read data from
- * @param start The offset of the first byte in the file that should be copied
- * @param length The length of bytes of data to copy from the file
- * @param mem_pointer The memory location to store the data read from the file
+ * @param fd A file descriptor for the file to read data from.
  *
- * @return an integer value,
- * if the value > 0 then the file was read successfully
- * if the value <= 0 then the file was read unsuccessfully
+ * @param start The offset of the first byte in the file that should be copied.
+ *
+ * @param length The length of bytes of data to copy from the file.
+ *
+ * @param mem_pointer The memory location to store the data read from the file.
+ *
+ * @return Returns an integer value, if the value > 0 then the file was
+ * read successfully. If the value <= 0 then the file failed to be read.
  *
  * @exception FILE_LENGTH_OVERRUN If the file cannot be read correctly because
  * the length of bytes specified exceeds the length of the file, no data will be
  * copied from the file if an overrun occurs.
  *
  * @exception INVALID_FILE_DESCRIPTOR If the file descriptor specified does not
- * exist
+ * exist.
+ *
+ * @author Daniel Smullen
+ *
+ * @author Jon Gillett
+ *
+ * @author Joseph Heron
+ *
+ * @copyright GNU General Public License V3
  */
 extern int sfs_read(int fd, int start, int length, byte *mem_pointer);
+
 
 #endif /* READ_FILE_H_ */
