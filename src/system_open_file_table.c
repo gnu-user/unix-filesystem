17df2c361974a9203de2e5109ad6dc0f9c099350
drspangle <dsmullen@outbreaklabs.com>
2012-11-26 15:18:10 -0500

    added licence header template SHOOP DE WOOP

--- src/system_open_file_table.c
+++ src/system_open_file_table.c
@@ -1,7 +1,25 @@
-/**
- * System wide open file table
- * Needs to have at least 5 instances (to have 5 files open at once)
- **/
+/*
+ * SneakyFS - A Secure UNIX File System
+ *
+ * Copyright (C) 2012, Jonathan Gillett, Joseph Heron, and Daniel Smullen
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "system_open_file_table.h"
 
 swoft system_open_tb = {0};
