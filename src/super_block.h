b74350f497899c42b9bf14675376416dcb8736c0
drspangle <dsmullen@outbreaklabs.com>
2012-11-26 15:18:27 -0500

    added the licence header template

--- src/super_block.h
+++ src/super_block.h
@@ -1,12 +1,25 @@
-/**
- * The structures used for the super_block component of the file system:
+/*
+ * SneakyFS - A Secure UNIX File System
  *
- * (Integer) Size of disk (in blocks), which will take up 4 bytes of the block,
- * (Integer) Block size (in bytes), which will take up 4 bytes of the block,
- * A Pointer to the free block linked list, which will take 8 bytes,
- * A Pointer to the Inode table for the root directory, which will take 8 bytes,
- * (Integer) A Device id, which will take up 4 bytes.
-*/
+ * Copyright (C) 2012, Jonathan Gillett, Joseph Heron, and Daniel Smullen
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "glob_func.h"
 #include "error.h"
 
