2367522d59d828aaff1f624eb91acb4bbe9205f3
drspangle <dsmullen@outbreaklabs.com>
2012-11-26 04:14:23 -0500

    updated doxygen comments

--- src/get_size.h
+++ src/get_size.h
@@ -10,19 +10,27 @@
 #ifndef GET_SIZE_H_
 #define GET_SIZE_H_
 
-/** sfs_getsize
- * Get the size (in blocks) of the file with the pathname specified.
+
+/**
+ * @brief Get the size (in blocks) of the file with the pathname specified.
+ *
+ * @details Traverses the index block structure of a given file, reads all
+ * the locations inside, and the number of blocks is returned. If the file is a
+ * data file, the size of the file is returned. If the file is a directory,
+ * the number of elements within the directory will be returned.
  *
  * @param pathname The pathname of the file or directory, must be full directory
- * path
- * If it is a file then the size of the file will be returned
- * If it is a directory the number of entries will be returned
+ * path. Relative paths are not accepted.
  *
- * @return the size of the given file
- * If the value < 0 then an error has occurred.
+ * @return Returns the size of the given file. If the value < 0 then
+ * an error occurred.
  *
- * @exception FILE_NOT_FOUND If the pathname does not already exist
+ * @exception FILE_NOT_FOUND If the file at the given pathname does not exist.
+ *
+ * @exception INVALID_PATH If if the path specified is invalid, incomplete,
+ * or does not exist.
  */
 extern int sfs_getsize(char *pathname);
+
 
 #endif /* GET_SIZE_H_ */
