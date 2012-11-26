d446cde36766cc20a0c6bb32f9071675a6d757ca
drspangle <dsmullen@outbreaklabs.com>
2012-11-26 15:19:39 -0500

    blap

--- src/initialize.c
+++ src/initialize.c
@@ -1,9 +1,31 @@
-/**
+/*
+ * SneakyFS - A Secure UNIX File System
+ *
+ * Copyright (C) 2012, Jonathan Gillett, Joseph Heron, and Daniel Smullen
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
  * Initialize the hard disk bytes to 0 
  * Pre-defined block size
  * Pre-defined bytes per block
  * Initialize the super block
- **/
+ */
 #include "initialize.h"
 #include "block_func.h"
 #include "super_block.h"
