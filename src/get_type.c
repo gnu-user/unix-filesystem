1d0ad10e6d0adbb96d10b2262d72236cf1be04ad
gnu_user <gsoc.student@gmail.com>
2012-11-26 20:08:03 -0500

    REACTORED source code and comments

--- src/get_type.c
+++ src/get_type.c
@@ -24,47 +24,31 @@
 #include "traverse_tree.h"
 #include "get_type.h"
 
-/** sfs_gettype
- * Get the type of the file with the pathname specified.
- *
- * @param pathname The pathname of file to create, must be full directory path
- *
- * @return the file type of the given file
- * If the file type >= 0 then the file type retrieval is a success
- * 	If the file type is 0, or 1 then it is a file, directory respectively
- * 	If the file type > 1 then the file is not known
- * If the file type < 0 then the file type retrieval has failed
- *
- * @exception FILE_NOT_FOUND If the pathname does not already exist
- */
+
 int sfs_gettype(char *pathname)
 {
-	//TODO test gettype
 	uint32_t* inode_location = NULL;
 	char** tokens = NULL;
 
-	/**
-	 * Parse the pathname
-	 */
+	/* Parse the pathname */
 	tokens = tokenize_path(pathname);
 
 	if(tokens == NULL)
 	{
-		/**
+		/*
 		 * Invalid path name
 		 * TODO validate this error code
 		 */
 		print_error(INVALID_PATH);
 		return 0;
 	}
-	/**
-	 * Traverse the file system to find the desired inode
-	 */
+
+	/* Traverse the file system to find the desired inode */
 	inode_location = traverse_file_system(tokens, false);
 
 	if(inode_location == NULL)
 	{
-		/**
+		/*
 		 * Invalid path or file/directory not found
 		 * TODO validate this error code
 		 */
@@ -72,7 +56,7 @@ int sfs_gettype(char *pathname)
 		return -1;
 	}
 
-	/**
+	/*
 	 * Retrieve the file type from the Inode
 	 * return the file type from the Inode
 	 *
