5116518e55a1662eb1b67057e79ca8185a0d7d71
gnu_user <gsoc.student@gmail.com>
2012-11-26 20:17:57 -0500

    REACTORED open_file source code and comments

--- src/open_file.c
+++ src/open_file.c
@@ -24,29 +24,15 @@
 #include "traverse_tree.h"
 #include "open_file.h"
 
-/** sfs_open
- * Opens the file specified by the pathname, if the file is successfully opened
- * a file descriptor is returned.
- *
- * @param pathname The full pathname of a file
- *
- * @return A file descriptor for the file opened
- * If the fd >= 0 then the file has successfully opened.
- * If the fd < 0 then the file has failed to open.
- *
- * @exception FILE_NOT_FOUND If the file specified in pathname does not exist
- */
+
 int sfs_open(char *pathname)
 {
-	//TODO fix open error when file not found (it might be seg faulting, program just crashed)
 	uint32_t* inode_location = NULL;
 	char** tokens = NULL;
 	inode* node = NULL;
 	byte* buf = NULL;
 
-	/*
-	 * Parse the pathname
-	 */
+	/* Parse the pathname */
 	tokens = tokenize_path(pathname);
 	if(tokens == NULL)
 	{
@@ -128,16 +114,7 @@ int sfs_open(char *pathname)
 	return show_information(add_to_swoft(inode_location[0]));
 }
 
-/**
- * The all of the information stored in the given file descriptor's Inode.
- *
- * @param fd integer, the file descriptor.
- *
- * @return an integer value, Adjusted to output the fd if success to allow for
- * it to be used on the return line
- * if the value >= 0 then the function is successful
- * if the value < 0 then the function is unsuccessful
- */
+
 int show_information(int fd)
 {
 	char str[100];
