0e799409bd177c7ba66e412f1548e892b34adfa2
gnu_user <gsoc.student@gmail.com>
2012-11-26 20:24:50 -0500

    REACTORED write_file source code and comments

--- src/write_file.c
+++ src/write_file.c
@@ -68,6 +68,7 @@ block* modify_data(int32_t start, uint32_t length, byte* data_buf, byte* actual_
 	return data_blocks;
 }
 
+
 block* segment_data(byte* data_buf)
 {
 	uint32_t i = 0;
@@ -155,29 +156,35 @@ block* segment_data_len(byte* data_buf, uint32_t length)
 }
 
 
-/** sfs_write
- * Write the length bytes of data specified from a memory location to the
- * specified file. The parameter start gives the offset of the first byte in
- * the file that should be copied to. Alternatively, the value of start may be
- * set to -1, this indicates that the specified number of bytes should be
- * appended to the file. Appending (setting start to -1) is the only allowable
- * way to increase the length of a file.
+/*
+ * High level pseudocode for sfs_write, refer to the function prototype
+ * in write_file.h for information about calling/using the function.
  *
- * @param fd A file descriptor for the file to write data to
- * @param start The offset of the first byte in the file that should be written
- * to, if start is -1 then the data will be appended to the file
- * @param length The length of bytes of data to be written to the file
- * @param mem_pointer The memory location containing the bytes to write to file
+ * blocks_needed = 1 //FOR INODE
+ * data_blocks = iterate_index(inode.location)
+ * data_buf = read_in_data(data_blocks)
  *
- * @return an integer value,
- * If the value > 0 the file write was a success
- * If the value <= 0 the file write was unsuccessful
+ * if start > 0
+ * 	if start + length > length of data_blocks in bytes
+ * 		return invalid write past file size
+ * 	data_buf = data_buf{until_start} conact mem_pointer conact data_buf
+ * 	after start + length}
+ * if start == -1
+ * 	data_buf = data_buf{all} concat mem_pointer
  *
- * @exception INVALID_FILE_DESCRIPTOR If the file descriptor specified does not
- * exist
+ * divide into blocks(data_buf)
+ * data += get number of data blocks
+ * if(calc_num_free_blocks(data + num_of_index_blocks(data-1))) == NULL) //1 for the inode
+ * 	return 0;
+ * inode_location = get_free_block();
+ * data_blocks data_location = generate_index(d)
  *
- * @exception INSUFFICIENT_DISK_SPACE If the length of the blocks to be written
- * is greater than the amount of available blocks on disk
+ * int i = 0;
+ * while(data_location[i] != NULL)
+ * 	write_block(data_location.data_blocks[i], databuf[i])
+ * 	i++;
+ * populate_inode (data_location.index_block)
+ * link parent's index block to inode
  */
 int sfs_write(int fd, int start, int length, byte *mem_pointer)
 {
@@ -199,7 +206,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 	int retval = 0;
 	int i = 0;
 
-	//TODO split this into two different error codes/returns
+	/* TODO split this into two different error codes/returns */
 	if(fd >= 0 && fd < NUMOFL && length > 0 && start >= -1)
 	{
 		/*
@@ -216,35 +223,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 			return 0;
 		}
 
-		/* Based off a night of sleep and ze cudes
-		 *
-		 * blocks_needed = 1 //FOR INODE
-		 * data_blocks = iterate_index(inode.location)
-		 * data_buf = read_in_data(data_blocks)
-		 *
-		 * if start > 0
-		 * 	if start + length > length of data_blocks in bytes
-		 * 		return invalid write past file size
-		 * 	data_buf = data_buf{until_start} conact mem_pointer conact data_buf
-		 * 	after start + length}
-		 * if start == -1
-		 * 	data_buf = data_buf{all} concat mem_pointer
-		 *
-		 * divide into blocks(data_buf)
-		 * data += get number of data blocks
-		 * if(calc_num_free_blocks(data + num_of_index_blocks(data-1))) == NULL) //1 for the inode
-		 * 	return 0;
-		 * inode_location = get_free_block();
-		 * data_blocks data_location = generate_index(d)
-		 *
-		 * int i = 0;
-		 * while(data_location[i] != NULL)
-		 * 	write_block(data_location.data_blocks[i], databuf[i])
-		 * 	i++;
-		 * populate_inode (data_location.index_block)
-		 * link parent's index block to inode
-		 */
-		blocks_needed++; //Inode block space
+		blocks_needed++; /* Inode block space */
 		inode_write = get_swoft_inode(fd);
 		inode_loc = get_inode_loc(fd);
 		data_block_locations = iterate_index(inode_write.location, NULL);
@@ -259,6 +238,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 			 */
 			return -1;
 		}
+
 		if(data_block_locations == NULL)
 		{
 			/*
@@ -317,10 +297,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 			}
 		}
 
-		/*
-		 * Add the data to the data_buf
-		 * Break it back into blocks
-		 */
+		/* Add the data to the data_buf Break it back into blocks */
 		data_block = modify_data(start, length, data_buf, mem_pointer);
 
 		/*
@@ -336,9 +313,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 			return 0;
 		}
 
-		/*
-		 * Update the file size
-		 */
+		/* Update the file size */
 		inode_write.file_size = blocks_needed-1;
 
 		i = 0;
@@ -353,9 +328,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 		 */
 		while(i < blocks_needed -1)
 		{
-			/*
-			 * Get data_blocks
-			 */
+			/* Get data_blocks */
 			data_location[i] = get_free_block();
 			retval = write_block(data_location[i], data_block[i]);
 
@@ -380,9 +353,7 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 			i++;
 		}
 
-		/*
-		 * Rebuild the Index block
-		 */
+		/* Rebuild the Index block */
 		first_index = rebuild_index(data_location);
 
 		if(first_index == NULL)
@@ -467,7 +438,8 @@ int sfs_write(int fd, int start, int length, byte *mem_pointer)
 	 * TODO split this into two different returns/error codes
 	 * TODO validate this error code
 	 */
-	//print_error(INVALID_FILE_DESCRIPTOR);
-	//print_error(FILE_PAST_EOF);
+	/* print_error(INVALID_FILE_DESCRIPTOR);
+	 * print_error(FILE_PAST_EOF);
+	 */
 	return 0;
 }
